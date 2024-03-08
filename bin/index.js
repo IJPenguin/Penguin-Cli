5499eae995cc1eccd52815bcd9caf4c6771c14b8
Penguin <ishanjaiswal370@gmail.com>
2024-03-08 22:28:20 +0530

    added: search

--- bin/index.js
+++ bin/index.js
@@ -6,28 +6,56 @@ import inquirer from "inquirer";
 import ora from "ora";
 import "dotenv/config";
 import { Command } from "commander";
+import axios from "axios";
+
 import * as fs from "fs";
+
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
 const program = new Command();
 const configPath = process.env.CONFIG_PATH || "config.json";
 
-figlet.text(
-  "Penguin",
-  {
-    font: "Big",
-    horizontalLayout: "full",
-    verticalLayout: "full",
-    width: 200,
-    whitespaceBreak: true,
-  },
-  (err, data) => {
-    if (err) {
-      console.log("Something went wrong 🐧");
-      console.dir(err);
-      return;
-    }
-    console.log(chalk.cyanBright(data));
-  }
-);
+let animeName = "";
+let page = 1;
 
+const start = async () => {
+	const startText = await figlet("Penguin", "Graffiti");
+	console.log(chalk.blue(startText), "\n");
+};
 
+const spinner = async (startText, succeedText) => {
+	const spinner = ora(startText).start();
+	await sleep(1000);
+	spinner.color = "blue";
+	spinner.text = succeedText;
+	await sleep(1500);
+	spinner.stop();
+};
+
+const askTitle = async () => {
+	const askAnimeName = await inquirer.prompt({
+		name: "animeName",
+		type: "input",
+		message: "Enter Anime Name 🐧",
+	});
+	animeName = askTitle.title;
+};
+
+const getLink = async (animeName, page) => {
+	const query = axios
+		.get(`http://localhost:4000/anime/search?q=${animeName}&page=${page}`)
+		.then((res) => {
+			const data = res.data;
+			console.log(data.animes);
+			let i = 0;
+			for (const anime of data.animes) {
+				i++;
+				console.log(`${i} - ${anime.name}`);
+			}
+		});
+};
+
+await spinner("Waking up Penguin 🐧", "Penguin has woken up 🐧");
+await start();
+await askTitle();
+await getLink(animeName, page);
